85a3cbffd30fcbe66500b9cbb714707cbb37867b
Steven <gonghaima@hotmail.com>
2019-11-29 12:16:59 +1100

    more mene data

--- src/App.js
+++ src/App.js
@@ -1,41 +1,57 @@
-import React from 'react'
-import Sidebar from './Sidebar'
+import React from 'react';
+import HomeIcon from "@material-ui/icons/Home";
+import ReceiptIcon from "@material-ui/icons/Receipt";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import DesktopWindowsIcon from "@material-ui/icons/DesktopWindows";
+import SettingsIcon from "@material-ui/icons/Settings";
+import Sidebar from './Sidebar';
+
+function onClick(e, item) {
+    window.alert(JSON.stringify(item, null, 2));
+}
 
 const items = [
-    { name: 'home', label: 'Home' },
+    { name: "home", label: "Home", Icon: HomeIcon },
     {
-        name: 'billing',
-        label: 'Billing',
+        name: "billing",
+        label: "Billing",
+        Icon: ReceiptIcon,
         items: [
-            { name: 'statements', label: 'Statements' },
-            { name: 'reports', label: 'Reports' },
-        ],
+            { name: "statements", label: "Statements", onClick },
+            { name: "reports", label: "Reports", onClick }
+        ]
     },
+    "divider",
     {
-        name: 'settings',
-        label: 'Settings',
+        name: "settings",
+        label: "Settings",
+        Icon: SettingsIcon,
         items: [
-            { name: 'profile', label: 'Profile' },
-            { name: 'insurance', label: 'Insurance' },
+            { name: "profile", label: "Profile" },
+            { name: "insurance", label: "Insurance", onClick },
+            "divider",
             {
-                name: 'notifications',
-                label: 'Notifications',
+                name: "notifications",
+                label: "Notifications",
+                Icon: NotificationsIcon,
                 items: [
-                    { name: 'email', label: 'Email' },
+                    { name: "email", label: "Email", onClick },
                     {
-                        name: 'desktop',
-                        label: 'Desktop',
+                        name: "desktop",
+                        label: "Desktop",
+                        Icon: DesktopWindowsIcon,
                         items: [
-                            { name: 'schedule', label: 'Schedule' },
-                            { name: 'frequency', label: 'Frequency' },
-                        ],
+                            { name: "schedule", label: "Schedule" },
+                            { name: "frequency", label: "Frequency" }
+                        ]
                     },
-                    { name: 'sms', label: 'SMS' },
-                ],
-            },
-        ],
-    },
-]
+                    { name: "sms", label: "SMS" }
+                ]
+            }
+        ]
+    }
+];
+
 
 function App() {
     return (
